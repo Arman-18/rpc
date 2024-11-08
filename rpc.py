ef05d380e2d101311ffb4337281bddc7f07e9f12
Arman Shaikh <www.armanas@gmail.com>
2024-11-08 02:58:54 -0500

    Add loop for multiple rounds

--- rpc.py
+++ rpc.py
@@ -1,18 +1,14 @@
-def determine_winner(player, computer):
-    if player == computer:
-        return "It's a tie!"
-    elif (player == "rock" and computer == "scissors") or \
-         (player == "scissors" and computer == "paper") or \
-         (player == "paper" and computer == "rock"):
-        return "You win!"
-    else:
-        return "Computer wins!"
-
 def main():
     print("Welcome to the Rock, Paper, Scissors Game!")
     explain_rules()
-    player_choice = get_player_choice()
-    computer_choice = get_computer_choice()
-    print(f"Computer chose: {computer_choice}")
-    result = determine_winner(player_choice, computer_choice)
-    print(result)
+    while True:
+        player_choice = get_player_choice()
+        computer_choice = get_computer_choice()
+        print(f"Computer chose: {computer_choice}")
+        result = determine_winner(player_choice, computer_choice)
+        print(result)
+
+        play_again = input("Do you want to play again? (yes/no): ").lower()
+        if play_again != "yes":
+            print("Thank you for playing!")
+            break
